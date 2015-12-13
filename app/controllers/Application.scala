cb328aae0381ebe4d9989620710ea630bf9bff3d
OlegV <o.vitvitskyi@gmail.com>
2015-12-13 23:46:51 +0200

    Folowing tutorial

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,20 +1,31 @@
 package controllers
 
-import play.api._
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
+import models.Task
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
+
 
 class Application extends Controller {
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
 
   def index = Action {
     Redirect(routes.Application.tasks)
   }
 
   def tasks = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(Task.all(), taskForm))
   }
 
-  def addTask = TODO
+  def newTask = TODO
 
   def deleteTask(id: Long) = TODO
+
+
 
 }
